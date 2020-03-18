605cb18009934fe7818511bea9321135d0d82547
Jerish PJ <jerishonline@gmail.com>
2020-03-18 14:03:30 +0000

    Added functionality of Ienumerable

--- ConsoleProject/Program.cs
+++ ConsoleProject/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,6 +11,23 @@ namespace ConsoleProject
     {
         static void Main(string[] args)
         {
+            List<int> oyears = new List<int>();
+            oyears.Add(1990);
+            oyears.Add(1991);
+            oyears.Add(1993);
+            oyears.Add(2000);
+            oyears.Add(2001);
+            oyears.Add(2002);
+            oyears.Add(2003);
+
+            //Print The List Of Integers
+
+            IEnumerable<int> item = oyears;
+            foreach(int i in item)
+            {
+                Console.WriteLine(i);
+            }
+            Console.ReadLine();
         }
     }
 }
